c480869bebdc4ebab24fd4f6bd9cb5634ac49f0e
janco@tenacious.digital <janco@tenacious.digital>
2022-04-19 12:29:14 +0200

    SMC-4817: Removed bluebird from stop-cluster in happner-cluster

--- packages/happner-cluster/test/_lib/stop-cluster.js
+++ packages/happner-cluster/test/_lib/stop-cluster.js
@@ -1,20 +1,8 @@
-var Promise = require('bluebird');
-
-module.exports = function(servers, done) {
-  Promise.resolve(servers)
-    .map(
-      function(server) {
-        return server.stop({ reconnect: false }).then(function() {
-          // stopping all at once causes replicator client happn logouts to timeout
-          // because happn logout attempts unsubscribe on server, and all servers
-          // are gone
-          return Promise.delay(200); // ...so pause between stops (long for travis)
-        });
-      },
-      { concurrency: 1 }
-    ) // ...and do them one at a time
-    .then(function() {
-      done();
-    })
-    .catch(done);
+const delay = require('await-delay');
+module.exports = async function (servers, done) {
+  for (let server of servers) {
+    await server.stop({ reconnect: false });
+    await delay(200);
+  }
+  done();
 };
