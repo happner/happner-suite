dd715746168dd0ca025ac71073fa6214f2818392
Simon Bishop <simon@tenacious.digital>
2022-05-09 13:42:24 +0200

    TEN-114: ensured test file removal

--- packages/happner-2/test/integration/security/system-groups-stop-start.js
+++ packages/happner-2/test/integration/security/system-groups-stop-start.js
@@ -1,76 +1,85 @@
-require('../../__fixtures/utils/test_helper').describe({ timeout: 120e3 }, (test) => {
-  let mesh;
-  const Mesh = test.Mesh;
-  const client = new Mesh.MeshClient({ secure: true, port: 8003 });
-  const username = 'username';
-  const password = 'xxx';
-  const filename = test.createTempfilePath(__dirname, 'nedb', '../../tmp');
+['loki', 'nedb'].forEach((provider) => {
+  require('../../__fixtures/utils/test_helper').describe({ timeout: 120e3 }, (test) => {
+    let mesh;
+    const Mesh = test.Mesh;
+    const client = new Mesh.MeshClient({ secure: true, port: 8003 });
+    const username = 'username';
+    const password = 'xxx';
+    const filename = test.createTempfilePath(__dirname, provider, '../../tmp');
 
-  beforeEach(async () => {
-    if (mesh) {
-      await mesh.stop();
-    }
-    mesh = await Mesh.create({
-      name: 'system-groups-stop-start',
-      happn: {
-        secure: true,
-        adminPassword: password,
-        port: 8003,
-        services: {
-          data: {
-            config: {
-              datastores: [
-                {
-                  name: 'persist',
-                  provider: 'happn-db-provider-nedb',
-                  isDefault: true,
-                  settings: {
-                    filename,
+    beforeEach(async () => {
+      if (mesh) {
+        await mesh.stop();
+      }
+      mesh = await Mesh.create({
+        name: 'system-groups-stop-start',
+        happn: {
+          secure: true,
+          adminPassword: password,
+          port: 8003,
+          services: {
+            data: {
+              config: {
+                datastores: [
+                  {
+                    name: 'persist',
+                    provider: `happn-db-provider-${provider}`,
+                    isDefault: true,
+                    settings: {
+                      filename,
+                    },
                   },
-                },
-                {
-                  name: 'volatile_permissions',
-                  provider: 'happn-db-provider-nedb',
-                  patterns: ['/_SYSTEM/_SECURITY/_PERMISSIONS/*'],
-                },
-              ],
+                  {
+                    name: 'volatile_permissions',
+                    provider: `happn-db-provider-${provider}`,
+                    patterns: ['/_SYSTEM/_SECURITY/_PERMISSIONS/*'],
+                  },
+                ],
+              },
             },
           },
         },
-      },
+      });
+      await mesh.exchange.security.upsertUser({
+        username,
+        password,
+      });
     });
-    await mesh.exchange.security.upsertUser({
-      username,
-      password,
+
+    afterEach(async () => {
+      if (client) {
+        await client.disconnect();
+        await mesh.stop();
+      }
     });
-  });
 
-  afterEach(async () => {
-    if (client) {
-      await client.disconnect();
-      await mesh.stop();
-    }
-  });
-
-  after(async () => {
-    try {
-      test.commons.fs.unlinkSync(filename);
-    } catch (e) {
-      // do nothing
-    }
-  });
-
-  it('runs the mesh once and connects with the admin client', async () => {
-    await client.login({
-      username,
-      password,
+    after(async () => {
+      try {
+        test.commons.fs.unlinkSync(filename);
+        const lokiTempfile = test.commons.path
+          .dirname(filename)
+          .concat(test.commons.path.sep)
+          .concat('temp_')
+          .concat(test.commons.path.basename(filename));
+        //whack loki temp file
+        test.commons.fs.unlinkSync(lokiTempfile);
+      } catch (e) {
+        // do nothing
+      }
     });
-  });
 
-  it('restarts the mesh connects with the admin client', async () => {
-    await client.login({
-      username,
-      password,
+    it('runs the mesh once and connects with the admin client', async () => {
+      await client.login({
+        username,
+        password,
+      });
+    });
+
+    it('restarts the mesh connects with the admin client', async () => {
+      await client.login({
+        username,
+        password,
+      });
     });
   });
 });
