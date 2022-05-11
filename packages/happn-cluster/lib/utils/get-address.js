c499dd247d9593694c399b12111238e8ad134f5a
janco@tenacious.digital <janco@tenacious.digital>
2022-05-11 13:43:47 +0200

    TEN-X-node18: Removed IPV4 tag from happn-cluster/utils.get-address

--- packages/happn-cluster/lib/utils/get-address.js
+++ packages/happn-cluster/lib/utils/get-address.js
@@ -27,7 +27,7 @@ function getFirstAvailableAddress(interfaceId, logger, os) {
       found.forEach((interfaceItem, interfaceItemIndex) => {
         if (
           !interfaceItem.internal &&
-          interfaceItem.family === 'IPv4' &&
+          // interfaceItem.family === 'IPv4' &&
           interfaceItem.address.indexOf('169.254') !== 0
         ) {
           candidates.push({
