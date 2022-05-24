ac29fd921ef3f359254770dcf4b06258331bb739
southbite <simon@tenacious.digital>
2022-05-24 13:58:17 +0200

    Revert "Ten-X-Node-18: Added happn-util-crypto test"
    This reverts commit 5e80d3cf398ecbbc58e01e6b4f0a5874b5b63515.

--- packages/happn-3/lib/services/transport/service.js
+++ packages/happn-3/lib/services/transport/service.js
@@ -86,6 +86,7 @@ TransportService.prototype.createServer = function (config, app, log, callback) 
   if (keyFileExists && certFileExists) {
     options.key = this.fs.readFileSync(config.keyPath);
     options.cert = this.fs.readFileSync(config.certPath);
+    return this.__createHttpsServer(options, app, callback);
   }
 
   if (!keyFileExists && !certFileExists) {
