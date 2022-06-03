0fbd19308c3686b2710b024444414d839640d1e3
janco@tenacious.digital <janco@tenacious.digital>
2022-06-03 18:06:49 +0200

    TEN-12: Increased initial delay after  cluste-startup in rest test

--- packages/happner-cluster/test/_lib/stop-cluster.js
+++ packages/happner-cluster/test/_lib/stop-cluster.js
@@ -2,7 +2,7 @@ const delay = require('await-delay');
 module.exports = async function (servers, done) {
   for (let server of servers) {
     await server.stop({ reconnect: false });
-    await delay(200);
+    await delay(300);
   }
   if (done) return done();
 };
