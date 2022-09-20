49e4a6fdec24c9bd51a2295816f12362efc1ad56
Simon Bishop <simon@tenacious.digital>
2022-09-20 16:56:11 +0200

    TEN-132: fixed lint issue

--- packages/happner-2/test/unit/security/authorization-regex-matches.js
+++ packages/happner-2/test/unit/security/authorization-regex-matches.js
@@ -1,6 +1,5 @@
 const test = require('../../__fixtures/utils/test_helper').create();
 describe(test.testName(__filename, 3), function () {
-    //772ad802-f2d3-4b07-8252-7ad7d4ae61f5:/_exchange/responses/meshname/component/method1/5d27417a-13f0-4890-8c5f-c5a3501da5dc/0:set
   it('tests various regex matches', function () {
     let responseKey =
       '1549e063-ecdc-4463-8e2b-19915512668f:/_exchange/responses/meshname/component/method1/6792e00e-5ca8-4035-93a2-b91dc92ac40e/367687678:set';
