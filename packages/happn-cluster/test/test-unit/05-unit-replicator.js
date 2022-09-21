9bbcaace1e6061b842f0e8b1f8db3d3573558587
southbite <simon@tenacious.digital>
2022-09-21 12:54:39 +0200

    fix/TEN-126-reconnect-on-error (#88)
    Co-authored-by: janco@tenacious.digital <janco@tenacious.digital>
    Co-authored-by: JustinErenJaeger <soulcali789@gmail.com>

--- packages/happn-cluster/test/test-unit/05-unit-replicator.js
+++ packages/happn-cluster/test/test-unit/05-unit-replicator.js
@@ -11,7 +11,7 @@ require('../lib/test-helper').describe({ timeout: 30e3 }, function (test) {
     replicator.happn.services.orchestrator.members = {
       __self: {
         client: {
-          on: () => {},
+          on: test.sinon.stub(),
         },
       },
     };
@@ -22,123 +22,116 @@ require('../lib/test-helper').describe({ timeout: 30e3 }, function (test) {
   });
 
   it('can call the send function, security update - default interval', function (done) {
+    this.timeout(9e3);
     const replicator = new Replicator(mockOpts);
     let started;
     replicator.happn = new MockHappn('http', 9000);
     replicator.happn.services.orchestrator.members = {
       __self: {
         client: {
-          on: () => {},
+          on: test.sinon.stub().callsArgWith(2, undefined),
         },
       },
     };
     replicator.initialize({}, () => {
-      replicator.start();
       replicator.__replicate = (topic, batch) => {
-        test.expect(Date.now() - started > 3000).to.be(true);
-        test.expect(topic).to.be('/security/dataChanged');
-        test.expect(batch).to.eql({
-          'unlink-group': {
-            group1: {
-              additionalInfo: undefined,
-              changedData: {
-                path: '/test/group1',
+        test.log('in replicate...');
+        let errorHappened;
+        try {
+          test.expect(Date.now() - started > 25e2).to.be(true);
+          test.expect(topic).to.be('/security/dataChanged');
+          test.expect(batch).to.eql({
+            'unlink-group': {
+              group1: {
+                additionalInfo: undefined,
+                changedData: {
+                  path: '/test/group1',
+                },
               },
             },
-          },
-          'link-group': {
-            group1: {
+            'link-group': {
+              group1: {
+                additionalInfo: undefined,
+                changedData: {
+                  _meta: {
+                    path: '/test/group1',
+                  },
+                },
+              },
+            },
+          });
+          //this should be emptied out
+          test.expect(replicator.securityChangeset).to.eql([]);
+          test.expect(replicator.unbatchSecurityUpdate(batch)).to.eql([
+            {
               additionalInfo: undefined,
+              whatHappnd: 'link-group',
               changedData: {
                 _meta: {
                   path: '/test/group1',
                 },
               },
             },
-          },
-        });
-        //this should be emptied out
-        test.expect(replicator.securityChangeset).to.eql([]);
-        test.expect(replicator.unbatchSecurityUpdate(batch)).to.eql([
-          {
-            additionalInfo: undefined,
-            whatHappnd: 'link-group',
-            changedData: {
-              _meta: {
+            {
+              additionalInfo: undefined,
+              whatHappnd: 'unlink-group',
+              changedData: {
                 path: '/test/group1',
               },
             },
-          },
-          {
-            additionalInfo: undefined,
-            whatHappnd: 'unlink-group',
-            changedData: {
-              path: '/test/group1',
-            },
-          },
-        ]);
-        replicator.stop(done);
+          ]);
+        } catch (e) {
+          errorHappened = e;
+        }
+        replicator.stop(() => {
+          done(errorHappened);
+        });
       };
       started = Date.now();
-      replicator.send(
-        '/security/dataChanged',
-        {
-          whatHappnd: SD_EVENTS.UNLINK_GROUP,
-          changedData: { path: '/test/group1' },
-        },
-        () => {
-          test.expect(replicator.securityChangeset).to.eql([
-            {
-              whatHappnd: 'unlink-group',
-              changedData: { path: '/test/group1' },
-            },
-          ]);
-
-          replicator.send(
-            '/security/dataChanged',
-            {
-              whatHappnd: SD_EVENTS.UNLINK_GROUP,
-              changedData: { path: '/test/group1' },
-            },
-            () => {
-              test.expect(replicator.securityChangeset).to.eql([
-                {
-                  whatHappnd: 'unlink-group',
-                  changedData: { path: '/test/group1' },
-                },
-                {
-                  whatHappnd: 'unlink-group',
-                  changedData: { path: '/test/group1' },
-                },
-              ]);
-
-              replicator.send(
-                '/security/dataChanged',
-                {
-                  whatHappnd: SD_EVENTS.LINK_GROUP,
-                  changedData: { _meta: { path: '/test/group1' } },
-                },
-                () => {
-                  test.expect(replicator.securityChangeset).to.eql([
-                    {
-                      whatHappnd: 'unlink-group',
-                      changedData: { path: '/test/group1' },
-                    },
-                    {
-                      whatHappnd: 'unlink-group',
-                      changedData: { path: '/test/group1' },
-                    },
-                    {
-                      whatHappnd: 'link-group',
-                      changedData: { _meta: { path: '/test/group1' } },
-                    },
-                  ]);
-                }
-              );
-            }
-          );
-        }
-      );
+      replicator.start((e) => {
+        if (e) return done(e);
+        replicator.send(
+          '/security/dataChanged',
+          {
+            whatHappnd: SD_EVENTS.UNLINK_GROUP,
+            changedData: { path: '/test/group1' },
+          },
+          () => {
+            replicator.send(
+              '/security/dataChanged',
+              {
+                whatHappnd: SD_EVENTS.UNLINK_GROUP,
+                changedData: { path: '/test/group1' },
+              },
+              () => {
+                replicator.send(
+                  '/security/dataChanged',
+                  {
+                    whatHappnd: SD_EVENTS.LINK_GROUP,
+                    changedData: { _meta: { path: '/test/group1' } },
+                  },
+                  () => {
+                    test.expect(replicator.securityChangeset).to.eql([
+                      {
+                        whatHappnd: 'unlink-group',
+                        changedData: { path: '/test/group1' },
+                      },
+                      {
+                        whatHappnd: 'unlink-group',
+                        changedData: { path: '/test/group1' },
+                      },
+                      {
+                        whatHappnd: 'link-group',
+                        changedData: { _meta: { path: '/test/group1' } },
+                      },
+                    ]);
+                  }
+                );
+              }
+            );
+          }
+        );
+      });
     });
   });
 
@@ -149,7 +142,7 @@ require('../lib/test-helper').describe({ timeout: 30e3 }, function (test) {
     replicator.happn.services.orchestrator.members = {
       __self: {
         client: {
-          on: () => {},
+          on: test.sinon.stub(),
         },
       },
     };
