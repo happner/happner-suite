6c50172b41edcab04cc0f0c2364b5bbc3cd6645b
Simon Bishop <simon@tenacious.digital>
2022-09-28 16:54:21 +0200

    TEN-132: removed .only

--- packages/happner-2/test/integration/security/authorization-cache-responses-regex-mask.js
+++ packages/happner-2/test/integration/security/authorization-cache-responses-regex-mask.js
@@ -1,4 +1,4 @@
-require('../../__fixtures/utils/test_helper').describe({ timeout: 120e3, only: true }, (test) => {
+require('../../__fixtures/utils/test_helper').describe({ timeout: 120e3 }, (test) => {
   const adminClients = {};
   const clients = [];
   const meshes = {};
