cf986a97761a6b3594b13b0f932644aedddb178a
leebow <leebow@protonmail.com>
2022-10-18 12:08:00 +0200

    TEN-119: additional data config and connect config validation tests

--- packages/configuration/test/integration/validation/connect-config-validation-tests.js
+++ packages/configuration/test/integration/validation/connect-config-validation-tests.js
@@ -31,6 +31,83 @@ describe(helper.testName(), function () {
     helper.expect(result.valid).to.equal(false);
     helper.expect(result.errors[0].message).to.equal('must be string');
   });
+
+  it('invalidates connect config with missing cookieDomain', () => {
+    const cacheConfig = createValidConnectConfig();
+    delete cacheConfig.config.middleware.security.cookieDomain;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal("must have required property 'cookieDomain'");
+  });
+
+  it('invalidates connect config with invalid cookieDomain', () => {
+    const cacheConfig = createValidConnectConfig();
+    cacheConfig.config.middleware.security.cookieDomain = 12312321;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
+  });
+
+  it('validates connect config with missing exclusions', () => {
+    const cacheConfig = createValidConnectConfig();
+    delete cacheConfig.config.middleware.security.exclusions;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates connect config with invalid exclusions', () => {
+    const cacheConfig = createValidConnectConfig();
+    cacheConfig.config.middleware.security.exclusions = '';
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be array');
+  });
+
+  it('validates connect config with missing forbiddenResponsePath', () => {
+    const cacheConfig = createValidConnectConfig();
+    delete cacheConfig.config.middleware.security.forbiddenResponsePath;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates connect config with invalid forbiddenResponsePath', () => {
+    const cacheConfig = createValidConnectConfig();
+    cacheConfig.config.middleware.security.forbiddenResponsePath = 1243434;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
+  });
+
+  it('validates connect config with missing unauthorizedResponsePath', () => {
+    const cacheConfig = createValidConnectConfig();
+    delete cacheConfig.config.middleware.security.unauthorizedResponsePath;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates connect config with invalid unauthorizedResponsePath', () => {
+    const cacheConfig = createValidConnectConfig();
+    cacheConfig.config.middleware.security.unauthorizedResponsePath = 1243434;
+
+    let result = validator.validateConnectConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
+  });
 });
 
 function createValidConnectConfig() {
--- packages/configuration/test/integration/validation/data-config-validation-tests.js
+++ packages/configuration/test/integration/validation/data-config-validation-tests.js
@@ -3,13 +3,161 @@ const helper = require('happn-commons-test/lib/base-test-helper').create();
 const ConfigValidator = require('../../../lib/validators/config-validator');
 
 describe(helper.testName(), function () {
+  const validator = new ConfigValidator();
+
   it('validates full data config', () => {
-    const validator = new ConfigValidator();
     const config = createValidDataConfig();
 
     const result = validator.validateDataConfig(config);
 
     helper.expect(result.valid).to.equal(true);
+  });
+
+  it('validates data config with missing secure', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.secure;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid secure', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.secure = 12312321;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be boolean');
+  });
+
+  it('validates data config with missing datastore name', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].name;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid datastore name', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].name = 390412034723894;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
+  });
+
+  it('invalidates data config with missing data provider', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].provider;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal("must have required property 'provider'");
+  });
+
+  it('invalidates data config with invalid data provider', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].provider = 'invalid-provider';
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper
+      .expect(result.errors[0].message)
+      .to.equal(
+        'must match pattern "(happn-db-provider-elasticsearch|happn-db-provider-loki|happn-db-provider-mongo|happn-db-provider-nedb)"'
+      );
+  });
+
+  it('validates data config with missing default', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].isDefault;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid default', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].isDefault = 'should-be-boolean';
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be boolean');
+  });
+
+  it('validates data config with missing settings', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].settings;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('validates data config with missing settings.fsync', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].settings.fsync;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid settings.fsync', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].settings.fsync = 'should-be-boolean';
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be boolean');
+  });
+
+  it('validates data config with missing dbfile path', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].dbfile;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid dbfile path', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].dbfile = 8923791283;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
+  });
+
+  it('validates data config with missing file name', () => {
+    const cacheConfig = createValidDataConfig();
+    delete cacheConfig.config.datastores[0].filename;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(true);
+  });
+
+  it('invalidates data config with invalid file name', () => {
+    const cacheConfig = createValidDataConfig();
+    cacheConfig.config.datastores[0].filename = 8923791283;
+
+    let result = validator.validateDataConfig(cacheConfig);
+
+    helper.expect(result.valid).to.equal(false);
+    helper.expect(result.errors[0].message).to.equal('must be string');
   });
 });
 
@@ -20,7 +168,7 @@ function createValidDataConfig() {
       datastores: [
         {
           name: 'testName',
-          provider: 'testProvider',
+          provider: 'happn-db-provider-nedb',
           isDefault: true,
           settings: {
             fsync: true,
