2f4f6d0c80004fe1c61d2a4285cfca342236955c
Janco Yspeert <janco@tenacious.digital>
2022-11-09 14:54:31 +0200

    TEN-12: extended timeout in happner-cluster integration test 10

--- packages/happner-cluster/test/integration/10-cluster-broker-dynamic.js
+++ packages/happner-cluster/test/integration/10-cluster-broker-dynamic.js
@@ -7,7 +7,7 @@ const testlightclient = require('../_lib/client-light');
 const getSeq = require('../_lib/helpers/getSeq');
 
 const clearMongoCollection = require('../_lib/clear-mongo-collection');
-require('../_lib/test-helper').describe({ timeout: 120e3 }, (test) => {
+require('../_lib/test-helper').describe({ timeout: 150e3 }, (test) => {
   const servers = [];
   let localInstance;
 
