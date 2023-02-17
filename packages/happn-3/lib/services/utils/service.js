07e3afe2314f946e350b84cc463a67d822e27ec3
Janco Yspeert <janco@tenacious.digital>
2023-02-17 13:04:40 +0200

    feat/SMC4817: Removed getFreePort from happn utils service

--- packages/happn-3/lib/services/utils/service.js
+++ packages/happn-3/lib/services/utils/service.js
@@ -1,7 +1,6 @@
 let commons = require("happn-commons"),
   fastClone = commons.fastClone,
   ms = require("ms"),
-  net = require("net"),
   happnUtils = commons.utils,
   fs = commons.fs,
   LRU = commons.lruCache,
@@ -288,15 +287,4 @@ UtilsService.prototype.getTemplatedPathCombinations = function (template, contex
 
   return templates;
 };
-UtilsService.prototype.getFreePort = async function () {
-  return new Promise((res, rej) => {
-    const srv = net.createServer();
-    srv.listen(0, () => {
-      const port = srv.address().port;
-      srv.close((err) => {
-        if (err) return rej(err);
-        res(port);
-      });
-    });
-  });
-};
+
