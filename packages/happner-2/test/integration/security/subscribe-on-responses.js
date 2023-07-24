ee6b5cc4de1d6f5aac46da10ab2fe8c6ef1bdaac
Simon Bishop <simon@tenacious.digital>
2023-07-24 16:40:17 +0200

    SMCGT-1556: added response leak test

---
+++ packages/happner-2/test/integration/security/subscribe-on-responses.js
@@ -0,0 +1,37 @@
+require('../../__fixtures/utils/test_helper').describe({ timeout: 120e3 }, (test) => {
+  test.createServersBefore([
+    {
+      secure: true,
+      happn: {
+        name: 'test-instance',
+        services: {
+          security: {
+            config: {
+              allowAnonymousAccess: true,
+            },
+          },
+        },
+      },
+    },
+  ]);
+  test.createHappnerUserBefore('test', 'test');
+  test.createHappnerSessionBefore('test', 'test');
+  it('ensures responses are not leaked', async () => {
+    const anonymousSession = await test.createHappnerSession({ username: '_ANONYMOUS' });
+    let eventHappened = false;
+    await anonymousSession.data.on(
+      '/_exchange/responses/test-instance/security/changePassword/*/*',
+      (data) => {
+        test.log('response leaked: ', data);
+        eventHappened = true;
+      }
+    );
+    await test.sessions[0].exchange.security.changePassword('test', 'test1');
+    await anonymousSession.disconnect();
+    if (eventHappened) {
+      throw new Error('test failed');
+    }
+  });
+  test.destroySessionsAfter();
+  test.destroyServersAfter();
+});
